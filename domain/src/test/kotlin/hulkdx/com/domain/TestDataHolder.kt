27a9bb222e028723a24098284bfebe99bf18c985
Mohammad Jafarzadeh Rezvan <hulktroll@gmail.com>
2019-07-20 15:24:42 +0300

    Unit Test fixed.

--- domain/src/test/kotlin/hulkdx/com/domain/TestDataHolder.kt
+++ domain/src/test/kotlin/hulkdx/com/domain/TestDataHolder.kt
@@ -7,8 +7,8 @@ import hulkdx.com.domain.entities.UserEntity
  * Created by Mohammad Jafarzadeh Rezvan on 17/07/2019.
  */
 
-val TEST_USER_1  = UserEntity(1,"firstName1", "lastName1", "email1", "avatarImageUrl1")
-val TEST_USER_2  = UserEntity(2,"firstName2", "lastName2", "email2", null)
+val TEST_USER_1  = UserEntity(1,"username1","firstName1", "lastName1", "email1", "avatarImageUrl1")
+val TEST_USER_2  = UserEntity(2,"username2","firstName2", "lastName2", "email2", null)
 
 val TEST_CLOTH_1 = ClothEntity(1,"imageUrl1", 10F, "EURO", TEST_USER_1)
 val TEST_CLOTH_2 = ClothEntity(2,"imageUrl2", 20F, "EURO", TEST_USER_2)
